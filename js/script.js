4f8a5449f4b7cb78688bff001ebbde3714a3bad5
su24-he <suwenh2@uci.edu>
2024-03-05 14:34:52 -0800

    updat

--- js/script.js
+++ js/script.js
@@ -1,58 +1,14 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
 
-$(document).ready(function() {
+$(document).ready(function () {
+    $('#image5-container').hide();
 
-    $("#cat-img").hide();
-    $("#happy-dog").hide();
-    $("#secret-text").hide();
-    
-
-
-    $("#doggo-img").click(function(){
-        $("#doggo-img").hide();
-        $("#cat-img").show();
-
+    // Add a click to image4-container
+    $('#image4-container').on('click', function () {
+        // Fade out Image 4
+        $(this).fadeOut('slow', function () {
+            // After fade out, fade in Image 5
+            $('#image5-container').fadeIn('slow');
+        });
     });
-
-    $("#cat-img").click(function(){
-        // $("#doggo-img").show();
-        $("#cat-img").hide();
-        $("#happy-dog").show();
-
-    });
-
-    $("#happy-dog").click(function(){
-        $("#doggo-img").show();
-        $("#cat-img").hide();
-        $("#happy-dog").hide();
-
-    });
-
-    $("#toggle-text").click(function(){
-        $("#secret-text").toggle();
-
-    });
-
-    $("#change-bg-color").click(function(){
-        $("#home-screen").css("background-color","khaki");
-
-        // change the font color of the text.
-        $("#change-bg-color").css("color","white");
-
-    });
-
-    $("#secret-text").click(function(){
-        $(".main-img").addClass("animate-img");
-        
-        setTimeout(function(){
-            $(".main-img").removeClass("animate-img");
-        },2000);
-
-    });
-    
-
-
-
-
-
 });
