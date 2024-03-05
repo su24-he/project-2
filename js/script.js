660bee63f2dda9a8905579b25b8d9db33573e2a4
su24-he <suwenh2@uci.edu>
2024-03-05 14:55:48 -0800

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
 
 $(document).ready(function () {
-    $('#image5-container').hide();
 
-    // Add a click to image4-container
-    $('#image4-container').on('click', function () {
-        // Fade out Image 4
-        $(this).fadeOut('slow', function () {
-            // After fade out, fade in Image 5
-            $('#image5-container').fadeIn('slow');
+    $('#image5-container').hide();
+     // Add a click to image4-container
+     $('#image4-container').on('click', function () {
+        //  fading in and fading out Image 4
+        $(this).fadeToggle('slow', function () {
+            //  fading in and fading out Image 5
+            $('#image5-container').fadeToggle('slow');
         });
     });
+});
+
+$('#video-container').on('click', function () {
+    // Hide Image 5 and fade in the video
+    $('#image4-container').show('slow');
+    $('#image5-container').fadeToggle('slow');
+
 });
