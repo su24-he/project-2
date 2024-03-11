4ec992a398bbc24646b9bddfdb96f7391a37e0cf
su24-he <suwenh2@uci.edu>
2024-03-10 21:17:25 -0700

    update

--- js/script.js
+++ js/script.js
@@ -3,15 +3,36 @@
 $(document).ready(function () {
 
     $('#image5-container').hide();
-     // Add a click to image4-container
-     $('#image4-container').on('click', function () {
+    $('#image6-container').hide();
+    $('#image7-container').hide();
+
+
+    // Add a click to image4-container
+    $('#image4-container').on('click', function () {
         //  fading in and fading out Image 4
         $(this).fadeToggle('slow', function () {
             //  fading in and fading out Image 5
             $('#image5-container').fadeToggle('slow');
         });
     });
+
+
+    // Add a click to image2-container 
+    $('#image2-container').click (function () {
+        $('#image6-container').fadeToggle('slow');
+        
+    });
+
+    $('#image2-container').click (function () {
+        $('#image7-container').fadeToggle('slow');
+        
+    });
+   
+
 });
+
+    
+
 
 $('#video-container').on('click', function () {
     // Hide Image 5 and fade in the video
